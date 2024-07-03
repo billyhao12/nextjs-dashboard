9d0257a316cd270548b497f523872d1e968dc997
Billy Hao <billyhao12@gmail.com>
2024-07-02 21:27:46 -0700

    add a secondary font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,4 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
+export const luistana = Lusitana({ subsets: ['latin'], weight: "400" });
